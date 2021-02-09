4c1355f6eda121acda0aec80cd877d2580db7bc5
Sreejith CR <sreejithcr2@gmail.com>
2021-02-09 18:52:48 +0530

    Refactored packages

--- app/src/main/java/com/codit/interview/aptitude/ad/AdHelper.java
+++ app/src/main/java/com/codit/interview/aptitude/ad/AdHelper.java
@@ -12,7 +12,7 @@ public class AdHelper {
 
     private static AdHelper instance;
     private InterstitialAd mInterstitialAd;
-    private static final String AD_ID = "ca-app-pub-7040172378865675/6346743007";
+    private static final String AD_ID = "ca-app-pub-3940256099942544/1033173712";
     private static final String TEST_ID = "ca-app-pub-3940256099942544/1033173712";
     private int counter = 0;
     private static final int frequency = 3;
